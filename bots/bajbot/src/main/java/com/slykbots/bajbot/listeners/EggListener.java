a7b379d4e66e4d9f13d6c58ef3f39f275eeb1ad4
slyk26 <102811034+slyk26@users.noreply.github.com>
2024-03-25 18:32:18 +0100

    dont clog logs

--- bots/bajbot/src/main/java/com/slykbots/bajbot/listeners/EggListener.java
+++ bots/bajbot/src/main/java/com/slykbots/bajbot/listeners/EggListener.java
@@ -2,13 +2,18 @@ package com.slykbots.bajbot.listeners;
 
 import com.slykbots.components.listeners.MessageListener;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.exceptions.ErrorResponseException;
 
 public class EggListener extends MessageListener {
     public EggListener() {
         super(c -> {
-           if("337700906751754246".equals(c.getMessage().getAuthor().getId())){
-               c.getMessage().addReaction(Emoji.fromUnicode("\uD83C\uDFF3\uFE0F\u200D⚧\uFE0F")).queue();
-               c.getMessage().addReaction(Emoji.fromUnicode("\uD83E\uDD5A")).queue();
+            try {
+                if ("337700906751754246".equals(c.getMessage().getAuthor().getId())) {
+                    c.getMessage().addReaction(Emoji.fromUnicode("\uD83C\uDFF3\uFE0F\u200D⚧\uFE0F")).queue();
+                    c.getMessage().addReaction(Emoji.fromUnicode("\uD83E\uDD5A")).queue();
+                }
+            }catch(ErrorResponseException e){
+                //
             }
         });
     }
