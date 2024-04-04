c33b6c6bb58060288ba244ca6d83ea905a4fd694
slyk26 <102811034+slyk26@users.noreply.github.com>
2024-04-04 21:08:21 +0200

    chickenbot

--- bots/bajbot/src/main/java/com/slykbots/bajbot/listeners/WhatListener.java
+++ bots/bajbot/src/main/java/com/slykbots/bajbot/listeners/WhatListener.java
@@ -8,13 +8,13 @@ import java.util.Arrays;
 public class WhatListener extends MessageListener {
     public WhatListener() {
         super(c -> {
-           var msg = c.getMessage();
-           var txt = msg.getContentDisplay().split(" ");
+            var msg = c.getMessage();
+            var txt = msg.getContentDisplay().split(" ");
 
-           if(Arrays.stream(txt).map(String::toLowerCase).anyMatch(t ->
-                           t.contains("que") ||
-                           (t.contains("what") && txt.length < 3)))
-               msg.addReaction(Emoji.fromCustom("chickenbutt", 1204210992377106502L, false)).complete();
+            if (Arrays.stream(txt).map(String::toLowerCase).anyMatch(t ->
+                    ("que".equals(t) ||
+                            (t.contains("what") && txt.length < 3))))
+                msg.addReaction(Emoji.fromCustom("chickenbutt", 1204210992377106502L, false)).complete();
 
         });
     }
